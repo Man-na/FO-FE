4b21ff4b5dfc1fdd503f169700e55725dfc5db61
홍종훈 <whdgnszz1@snu.ac.kr>
2025-02-03 22:14:30 +0900

    feat: 커스텀 버튼 제작

--- Manna/src/screens/auth/AuthHomeScreen.tsx
+++ Manna/src/screens/auth/AuthHomeScreen.tsx
@@ -1,8 +1,9 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {Button, SafeAreaView, View} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
 import {AuthStackParamList} from '../../navigation/stack/AuthStackNavigator';
-import {authNavigations} from '../../constants';
+import {authNavigations} from '../../constants/navigations';
+import CustomButton from '../../components/CustomButton';
 
 type AuthHomeScreenProps = StackScreenProps<
   AuthStackParamList,
@@ -13,12 +14,13 @@ function AuthHomeScreen({navigation}: AuthHomeScreenProps): React.JSX.Element {
   return (
     <SafeAreaView>
       <View>
-        <Button
-          title="로그인 화면으로 이동"
+        <CustomButton
+          label="로그인 하기"
           onPress={() => navigation.navigate(authNavigations.LOGIN)}
         />
-        <Button
-          title="회원가입 화면으로 이동"
+        <CustomButton
+          label="회원가입 하기"
+          variant="outlined"
           onPress={() => navigation.navigate(authNavigations.SIGNUP)}
         />
       </View>
