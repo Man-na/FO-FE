c8e3133fa4e4d8363362489c67133353d35f6963
홍종훈 <whdgnszz1@snu.ac.kr>
2025-02-05 14:36:40 +0900

    feat: 회원 관련 api 코드 작성

---
+++ Manna/src/service/auth/api.ts
@@ -0,0 +1,60 @@
+import {Category, Profile} from '@/types/domain';
+import {axiosInstance, getEncryptStorage} from '@/utils';
+
+type RequestUser = {
+  email: string;
+  password: string;
+};
+
+const postSignup = async ({email, password}: RequestUser): Promise<void> => {
+  const {data} = await axiosInstance.post('localhost:9000/api/v1/user/signup', {
+    email,
+    password,
+  });
+
+  return data;
+};
+
+type ResponseToken = {
+  accessToken: string;
+  refreshToken: string;
+};
+
+const postLogin = async ({
+  email,
+  password,
+}: RequestUser): Promise<ResponseToken> => {
+  const {data} = await axiosInstance.post('localhost:9000/api/v1/user/signup', {
+    email,
+    password,
+  });
+
+  return data;
+};
+
+type ResponseProfile = Profile & Category;
+
+const getProfile = async (): Promise<ResponseProfile> => {
+  const {data} = await axiosInstance.get('localhost:9000/api/v1/user/me');
+
+  return data;
+};
+
+const getAccessToken = async (): Promise<ResponseToken> => {
+  const refreshToken = await getEncryptStorage('refreshToken');
+
+  const {data} = await axiosInstance.get('localhost:9000/api/v1/user/refresh', {
+    headers: {
+      Authorization: `Bearer ${refreshToken}`,
+    },
+  });
+
+  return data;
+};
+
+const logout = async () => {
+  await axiosInstance.post('/auth/logout');
+};
+
+export {getAccessToken, getProfile, logout, postLogin, postSignup};
+export type {RequestUser, ResponseToken, ResponseProfile};
--- Manna/src/utils/index.ts
+++ Manna/src/utils/index.ts
@@ -1,3 +1,4 @@
+export * from './axios';
 export * from './common';
-export * from './validate';
 export * from './encryptStorage';
+export * from './validate';
