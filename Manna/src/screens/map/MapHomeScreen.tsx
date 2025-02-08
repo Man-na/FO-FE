d3ca64feb58436198695999e1824429cbd12c88e
홍종훈 <whdgnszz1@snu.ac.kr>
2025-02-08 15:01:48 +0900

    feat: 구글맵스 연동

--- Manna/src/screens/map/MapHomeScreen.tsx
+++ Manna/src/screens/map/MapHomeScreen.tsx
@@ -1,15 +1,31 @@
 import useAuth from '@/service/auth/queries/useAuth';
 import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, StyleSheet, View} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 function MapHomeScreen(): React.JSX.Element {
   const {logoutMutation} = useAuth();
   return (
-    <View>
-      <Text>맵 스크린</Text>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        followsUserLocation
+        showsMyLocationButton={true}
+      />
       <Button title="로그아웃" onPress={() => logoutMutation.mutate(null)} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
 
 export default MapHomeScreen;
--- Manna/src/utils/axios.ts
+++ Manna/src/utils/axios.ts
@@ -1,6 +1,12 @@
+import {Platform} from 'react-native';
 import axios from 'axios';
 
+const baseURL =
+  Platform.OS === 'ios'
+    ? 'http://localhost:9000/api/v1'
+    : 'http://10.0.2.2:9000/api/v1';
+
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:9000/api/v1',
+  baseURL,
   withCredentials: true,
 });
